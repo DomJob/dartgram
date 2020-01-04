40c97d9d959d83480c20c1db8a0a612504bc147e
DomJob <dominic.jobin@gmail.com>
2020-01-04 16:23:24 -0500

    answerCallbackQuery

--- lib/src/entities/callback_query.dart
+++ lib/src/entities/callback_query.dart
@@ -6,10 +6,24 @@ class CallbackQuery extends Entity {
   Message message;
   String data;
 
-  CallbackQuery(Bot bot, Map<String, dynamic> raw) : super(raw) {
+  final Bot _bot;
+
+  CallbackQuery(this._bot, Map<String, dynamic> raw) : super(raw) {
     id = raw['id'];
-    from = Entity.generate<User>(bot, raw['from']);
-    message = Entity.generate<Message>(bot, raw['message']);
+    from = Entity.generate<User>(_bot, raw['from']);
+    message = Entity.generate<Message>(_bot, raw['message']);
     data = raw['data'];
   }
+
+  Future<void> answer({String text,
+          bool show_alert = false,
+          String url,
+          int cache_time = 0}) =>
+      _bot.request('answerCallbackQuery', {
+        'callback_query_id': id,
+        'text': text,
+        'show_alert': show_alert,
+        'url': url,
+        'cache_time': cache_time
+      });
 }
